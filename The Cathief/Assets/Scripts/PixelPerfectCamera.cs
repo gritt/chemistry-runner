d1d542f17063d349888786b55cb0ae28dd7dc1c0
Gilvan <gilvanritter@gmail.com>
2017-04-30 20:03:29 -0300

    created class to calculate the pixel scaling according to resolution

--- The Cathief/Assets/Scripts/PixelPerfectCamera.cs
+++ The Cathief/Assets/Scripts/PixelPerfectCamera.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class PixelPerfectCamera : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	public static float pixelToUnit = 1f;
+	public static float scale = 1f;
+
+	public Vector2 nativeResolution = new Vector2(240, 160);
+
+
+	void Awake () {
+
+		// To calculate the game camera scale befor game starts
+
+		var camera = GetComponent<Camera> ();
+
+		if (camera.orthographic) {
+
+			scale = Screen.height / nativeResolution.y;
+
+			pixelToUnit *= scale;
+
+			camera.orthographicSize = (Screen.height / 2.0f) / pixelToUnit;
+		}
 	}
 }
