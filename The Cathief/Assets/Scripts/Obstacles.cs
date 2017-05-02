7a9111933a2ba83b0ab520f148711aaf724515a2
Gilvan <gilvanritter@gmail.com>
2017-05-01 22:29:14 -0300

    created obstacle class to handle obstacles logic, pick a random sprite when a object is recycled, fix for collider sizes for different objects

---
+++ The Cathief/Assets/Scripts/Obstacles.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour, IRecycle {
+
+	// To change the sprite each time a obstacle is restarted
+
+	public Sprite[] sprites;
+
+	public Vector2 colliderOffset = Vector2.zero;
+
+	public void Restart(){
+
+		// Randomically pick a sprite to use in the obstacle
+		var renderer = GetComponent<SpriteRenderer> ();
+		renderer.sprite = sprites [Random.Range (0, sprites.Length)];
+
+
+		// Resize collider boxes acoording to objects
+		var collider = GetComponent<BoxCollider2D> ();
+		var size = renderer.bounds.size;
+		size.y += colliderOffset.y;
+
+		collider.size = size;
+		collider.offset = new Vector2 (-colliderOffset.x, collider.size.y / 2 - colliderOffset.y);
+	}
+
+	public void Shutdown(){
+	
+	}
+}
