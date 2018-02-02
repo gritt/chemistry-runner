0052be6ef0792540d558e3597a0e34e26c8987c1
Gilvan <gilvanritter@gmail.com>
2018-02-02 02:15:33 -0200

    detect when the player is standing and make it jump!!

--- ChemistryRunner/Assets/Scripts/InputState.cs
+++ ChemistryRunner/Assets/Scripts/InputState.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class InputState : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public bool actionButton;
+	public float absVelX = 0f;
+	public float absVelY = 0f;
+
+	public bool standing;
+	public float standingThreshold = 1;
+
+	private Rigidbody2D body2d;
+
+	void Awake () {
+		body2d = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		actionButton = Input.anyKeyDown;
 	}
+
+	void FixedUpdate () {
+		absVelX = System.Math.Abs (body2d.velocity.x);
+		absVelY = System.Math.Abs (body2d.velocity.y);
+
+		standing = absVelY <= standingThreshold;
+	}		
 }
