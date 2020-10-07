8ad244cb2fd29d8f0b43a84597ccb642187feb0b
Udhay <udhaybrahmi@gmail.com>
2020-10-07 11:15:31 +0530

    Restart button added

--- ChemistryRunner/Assets/Scripts/Menu.cs
+++ ChemistryRunner/Assets/Scripts/Menu.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
     public void quit() {
         Application.Quit();
+    }
+    public void restart() {
+       SceneManager.LoadScene(0);
     }
 }
